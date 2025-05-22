263fb6eccfdb4ee7e852a1b7d1754e37c4a46eb9
kamalsarasq <kamalsarascc@gmail.com>
2025-05-22 18:25:49 +0530

    Create Http_Request_Handler.lua

---
+++ Http_Request_Handler.lua
@@ -0,0 +1,51 @@
+local HttpService = game:GetService("HttpService")
+
+-- // FUNCTION TO MANIPULATE STRING //
+local RequestHandler = {}
+RequestHandler.__index = RequestHandler
+
+-- // CREATOR FUNCTION //
+function RequestHandler.new(URL, API_KEY, HEADERS, BODY)
+	local newRequestHandler = {}
+	setmetatable(newRequestHandler, RequestHandler)
+	
+	-- properties
+	newRequestHandler.Url = URL
+	newRequestHandler.APIkey = API_KEY
+	newRequestHandler.Headers = HEADERS or {
+			["Authorization"] = "Bearer " .. newRequestHandler.APIkey,
+			["Content-Type"] = "application/json"
+	}
+	
+	newRequestHandler.Body = BODY
+	
+	return newRequestHandler
+end
+
+-- // REQUEST HANDLER //
+function RequestHandler:Request()
+	-- encoding body
+	self.Body = HttpService:JSONEncode(self.Body)
+	
+	-- sending request to ClosedAI
+	local response = HttpService:RequestAsync({
+		Url = self.Url,
+		Method = "POST",
+		Headers = self.Headers,
+		Body = self.Body
+	})
+	
+	-- handling error
+	if not response.Success then
+		error("Request failed: " .. response.StatusCode .. " - " .. response.StatusMessage)
+	end
+	
+	warn("success")
+	-- decoding response
+	local decodedResponse = HttpService:JSONDecode(response.Body)
+	-- getting the message
+	local chatGPTMessage = decodedResponse["choices"][1]["message"]["content"]
+	return chatGPTMessage -- retunring AI message
+end
+
+return RequestHandler
