56e3a475b7ba59839861a21454cec6cd14892044
kamalsarasq <kamalsarascc@gmail.com>
2025-05-22 18:24:53 +0530

    Create Loadstring.lua

---
+++ Loadstring.lua
@@ -0,0 +1,26 @@
+-- // VARIABLES //
+local module = {}
+
+local remote = game.ReplicatedStorage.SendServerMessage :: RemoteEvent
+local TextChatService = game:GetService("TextChatService")
+
+-- prefix
+local prefix = "[GOD] "
+
+-- // FUNCTION TO LOAD THE STRING //
+function module.load(str: string, player: Player)
+	local success, errormessage = pcall(function()
+		loadstring(str)()
+	end)
+	
+	-- if not success then we return
+	if not success then error(errormessage) return end
+	
+	-- sending message to every player on chat
+	remote:FireAllClients(
+		prefix .. "God listened to " .. player.DisplayName .. ".",
+		TextChatService.TextChannels.RBXSystem
+	)
+end
+
+return module
